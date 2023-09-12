2ab7445be0fe4fb496e648a61020c7b832a8facc
Amoghkulkarni13 <amoghronaldo07@gmail.com>
2023-09-13 00:32:13 +0530

    My first commit

---
+++ TestProject/src/Amogh/Ronaldo.java
@@ -0,0 +1,10 @@
+package Amogh;
+
+public class Ronaldo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ TestProject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Amogha
+ *
+ */
+module TestProject {
+}
